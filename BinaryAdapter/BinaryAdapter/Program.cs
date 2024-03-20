026a6737e5104a7e88f6d58af92f8476f6e65680
EspinozaJuan07 <150630261+EspinozaJuan07@users.noreply.github.com>
2024-03-20 08:36:57 -0600

    Add files via upload

---
+++ BinaryAdapter/BinaryAdapter/Program.cs
@@ -0,0 +1,15 @@
+﻿using BinaryAdapter;
+using System.Security.AccessControl;
+
+string filePath = "people.bin";
+
+IPersonRepository personRepository = new PersonBinaryRepository(filePath);
+
+//Agregar algunas personas
+personRepository.Add(new Person { Name = "Juan", Age = 17, Height = 170 });
+personRepository.Add(new Person { Name = "Alvaro", Age = 19, Height = 146.7 });
+personRepository.Add(new Person { Name = "Gabriel", Age = 35, Height = 152.5 });
+
+Console.WriteLine($"Datos de personas guardados en {filePath}");
+foreach (Person person in personRepository.GetAll())
+    Console.WriteLine($"Id: {person.Id}, Nombre: {person.Name}, Edad: {person.Age}, Estatura: {person.Height}");
