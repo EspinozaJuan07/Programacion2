7bc617f4d2909106beef92469d9954d92865ba83
EspinozaJuan07 <150630261+EspinozaJuan07@users.noreply.github.com>
2024-04-13 09:02:18 -0600

    Update Form1.cs

--- EjercicioTask/EjercicioTask/Form1.cs
+++ EjercicioTask/EjercicioTask/Form1.cs
@@ -2,11 +2,9 @@ namespace EjercicioTask
 {
     public partial class Form1 : Form
     {
-        private TaskScheduler _scheduler;
         public Form1()
         {
             InitializeComponent();
-            _scheduler = TaskScheduler.FromCurrentSynchronizationContext();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -16,25 +14,7 @@ namespace EjercicioTask
 
         private void btncalcular_Click(object sender, EventArgs e)
         {
-            int x;
-            decimal result;
-            x = Convert.ToInt32(maskedTextBox1.Text);
-            result = Convert.ToDecimal(Factorial(x));
-
-            txtresult.Text = result.ToString();
-            new Task(() =>
-            {
-                Task[] tareas =
-                {
-                Task.Factory.StartNew(() => Factorial(x),
-                CancellationToken.None,
-                TaskCreationOptions.LongRunning,
-                _scheduler),               
-            };
-                Task.WaitAll(tareas);
-
-            }).Start();
-
+        
         }
 
         private decimal Factorial(decimal x)
